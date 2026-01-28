ae6f637abc20ae6cdd5796b1ffc3c1575b6a0a43
rainyeung12022003-jpg <rainyeung12022003@gmail.com>
2026-01-28 10:47:03 +0800

    Add files via upload

---
+++ testing1.py
@@ -0,0 +1,67 @@
+# l = 30
+#
+# def add(x, y, a =0, b =10, c=100):
+#     global l
+#     z = l + y
+#     d = a + b + c
+#     print("z = " + str(z))
+#     print("d =" + str(d))
+#
+#     return l
+#
+# print(add(10,10,10))
+#
+# val = add(10,40,100)
+# print(val)
+#
+# print("hello world")
+
+
+y = 30
+# print(y)
+# print(float(y))
+# print(type(y))
+
+# z = x + y
+# print("z= " + str(z))
+
+# a, b, c = "apple", "cherry", "orange"
+# print(a + b + c)
+#
+# fruits = ("apple", "cherry", "orange")
+# fruits.append("pineapple")
+# fruits[0] = "mango"
+# print(len(fruits))
+# fruits.remove("cherry")
+# fruits.clear()
+# print(fruits)
+x = 10
+y = 20
+z =                                                                                                                    30
+
+if x > y and z > y:
+    print("true")
+else:
+    print("false")
+
+# import random
+# # print(random.randint(0,100))
+#
+# for x in range(10):
+#     print(random.randint(0, 100))
+#
+# while True:
+#     print(random.randint(0, 100))
+
+
+# number = 0
+#
+# while number < 50:
+#     print(random.randint(0,100))
+#     number += 1
+# print(fruits[0] + " " + fruits[2])
+
+# if 50 > 20:
+#     print("true")
+# else:
+#     print("false")
